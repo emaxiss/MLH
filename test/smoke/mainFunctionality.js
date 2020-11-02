01f9c86b535f6fb5525e0d7f957028a99fbb4b68
Maksim Evstafev <emaxiss@gmail.com>
2020-11-01 22:57:54 -0500

    added more display tests

---
+++ test/smoke/mainFunctionality.js
@@ -0,0 +1,17 @@
+const sel = require("../../data/selectors.json");
+const exp = require("../../data/expected.json");
+
+describe('Checking the main functionality', function () {
+
+    describe('Happy path', function () {
+
+        it('TC-021 Create button is clickable after 1-4 are filled in', function () {
+            browser.url('');
+            const name = $(sel.name).isDisplayed();
+            expect(name).toEqual(true);
+        });
+
+        it('TC-015 Gender (he)', function () {
+            const gender = $$(sel.gender)[0].isDisplayed();
+            expect(gender).toEqual(true);
+        });
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/smoke/*.js',
+        './test/smoke/inputs.js',
         './test/regression/*.js'
     ],
     // Patterns to exclude.
