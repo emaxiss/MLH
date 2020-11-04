51bcb4dea5ccb2f8a45265791a11f944154368af
Maksim Evstafev <emaxiss@gmail.com>
2020-11-04 00:24:04 -0500

    added helpers

--- test/smoke/mainFunctionality.js
+++ test/smoke/mainFunctionality.js
@@ -1,5 +1,7 @@
-const sel = require("../../data/selectors.json");
-const exp = require("../../data/expected.json");
+const sel = require ('../../data/selectors.json');
+const data = require ('../../data/testData.json');
+const inputValues4 = require ('../../helpers/inputValues4');
+const inputValues4Click = require ('../../helpers/inputValues4Click');
 
 describe('Checking the main functionality', function () {
 
@@ -7,13 +9,17 @@ describe('Checking the main functionality', function () {
 
         it('TC-021 Create button is clickable after 1-4 are filled in', function () {
             browser.url('');
-            let name = $(sel.name);
-            name.addValue("LadyBug");
-
-            expect(name).toEqual(true);
+            inputValues4(data.name, data.gender.she, data.age, data.storyType)
+            const create = $(sel.create).isEnabled();
+            expect(create).toEqual(true);
         });
 
-        it('TC-015 Gender (he)', function () {
-            const gender = $$(sel.gender)[0].isDisplayed();
-            expect(gender).toEqual(true);
+        it('TC-022 Click button and verify story generated', function () {
+            browser.url('');
+            inputValues4Click(data.name, data.gender.he, data.age, data.storyType)
+            const btn = $(sel.tryAgain).isDisplayed();
+            expect(btn).toEqual(true);
         });
+    });
+
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,13 +17,15 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        //'./test/smoke/inputs.js',
+        './test/smoke/inputs.js',
         './test/smoke/labels.js',
+        './test/smoke/mainFunctionality.js',
         './test/regression/*.js'
     ],
     // Patterns to exclude.
     exclude: [
-        // 'path/to/excluded/files'
+        './test/smoke/inputs.js',
+        './test/smoke/labels.js'
     ],
     //
     // ============
